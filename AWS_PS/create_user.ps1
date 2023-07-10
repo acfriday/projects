51f65a6739fcd87d4953489396a7cf27e2cc856d
ACF <82184168+acfriday@users.noreply.github.com>
2023-07-10 13:51:50 -0400

    Added password to new user

--- AWS_PS/create_user.ps1
+++ AWS_PS/create_user.ps1
@@ -1,7 +1,9 @@
-# Assign variables and use AWS profile at ~/.aws/credentials to authenticate
+# Assign variables and use AWS profile at ~/.aws/credential to authenticate
+Import-Module AWSPowerShell.NetCore
+
 $Creds = Set-AWSCredential -ProfileName 'projects'
-$Group = "developer"
-$Name = "Dev1"
+$Group = 'developer'
+$Name = 'Dev1'
 
 #Create New IAM User
 New-IAMUser -UserName $Name $Creds
@@ -16,4 +18,7 @@ Register-IAMGroupPolicy -Select ^GroupName $Group -PolicyArn arn:aws:iam::aws:po
 
 # Add New User to Group to gain permissions
 Add-IAMUserToGroup -UserName $Name -GroupName $Group
+
+# Added password by setting password as $PW variable in termnial for this session
+New-IAMLoginProfile -Select ^UserName $Name -Password ($PW).ToString() -PasswordResetRequired $false -Credential $Creds
 
