e2da5cd215264828460447399f9de10b1e6a2ff9
ACF <82184168+acfriday@users.noreply.github.com>
2023-07-18 20:56:13 -0400

    added random data

--- AWS_Python/api/api.py
+++ AWS_Python/api/api.py
@@ -1,10 +1,33 @@
 import flask
+from flask import request, jsonify
 
 app = flask.Flask(__name__)
 app.config['DEBUG'] = True
 
+teams = [
+    {
+        'id': 0,
+        'name': 'Lakers',
+        'championships': '17'},
+    {   
+        'id': 1,
+        'name': 'Celtics',
+        'championships': '17'},
+    {   
+        'id': 2,
+        'name': 'Bulls',
+        'championships': '6'}
+]
+
+
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>Test API</h1><p>This site is a prototype API for learning Flask and Python.</p>"
+
+
+@app.route('/api/v1/resources/teams/all', methods=['GET'])
+def api_all():
+    return jsonify(teams)
+
 
 app.run()
