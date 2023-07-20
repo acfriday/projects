0a4180e771fb5745693e30bd46636c65d40066e8
ACF <82184168+acfriday@users.noreply.github.com>
2023-07-20 00:29:08 -0400

    create dynamodb table

--- AWS_Python/api/api.py
+++ AWS_Python/api/api.py
@@ -1,23 +1,43 @@
+import os
+import boto3
 import flask
 from flask import request, jsonify
+
+
+os.environ['AWS_Profile'] = 'projects'
 
 app = flask.Flask(__name__)
 app.config['DEBUG'] = True
 
-teams = [
-    {
-        'id': 0,
-        'name': 'Lakers',
-        'championships': '17'},
-    {   
-        'id': 1,
-        'name': 'Celtics',
-        'championships': '17'},
-    {   
-        'id': 2,
-        'name': 'Bulls',
-        'championships': '6'}
-]
+
+dynamodb = boto3.resource('dynamodb')
+table_name = 'BBallTable'
+table = dynamodb.Table(table_name)
+
+if table == False:
+
+    table = dynamodb.create_table(
+        TableName = table_name,
+        KeySchema = [
+            {
+                'AttributeName': 'id',
+                'KeyType': 'HASH'
+            }   
+        ],
+        AttributeDefinitions = [
+            {
+                'AttributeName': 'id',
+                'AttributeType': 'S'
+            }
+        ],
+        ProvisionedThroughput = 
+        {
+            'ReadCapacityUnits': 5,
+            'WriteCapacityUnits': 5
+        }
+    )
+    table.meta.client.get_waiter('table_exists').wait(TableName = table_name)
+
 
 
 @app.route('/', methods=['GET'])
@@ -27,6 +47,8 @@ def home():
 
 @app.route('/api/v1/resources/teams/all', methods=['GET'])
 def api_all():
+    response = table.scan()
+    teams = response['items']
     return jsonify(teams)
 
 
