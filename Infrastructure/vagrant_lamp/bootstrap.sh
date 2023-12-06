faec18100b874e96a0e24d1d99cd48500e46480a
ACF <82184168+acfriday@users.noreply.github.com>
2023-12-05 20:57:51 -0500

    updates 2

--- Infrastructure/vagrant_lamp/bootstrap.sh
+++ Infrastructure/vagrant_lamp/bootstrap.sh
@@ -2,7 +2,3 @@ apt-get update
 
 # Install LAMP Server
 apt-get install -y lamp-server^
-
-# Docker Engine for Linux installation script from get.docker.com
-curl -fsSL get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
