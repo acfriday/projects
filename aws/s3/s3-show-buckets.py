a3efe46de2ff6340fdc18334eda095c8a8c26719
ACF <82184168+acfriday@users.noreply.github.com>
2023-12-28 22:09:30 -0500

    initial commit

---
+++ aws/s3/s3-show-buckets.py
@@ -0,0 +1,22 @@
+import os
+import boto3
+from botocore.exceptions import ClientError
+os.environ["AWS_Profile"] = "projects"
+
+def ListBucket():
+
+    try:
+        s3 = boto3.client('s3')
+        response = s3.list_buckets()
+        if not response:
+             print("No buckets available to this user")
+        else:
+             print("##Listing available buckets for this account##")
+
+    except ClientError as e:
+        print(f"Error: {e}")
+
+    for bucket in response['Buckets']:
+        print(f"Bucket: '{bucket['Name']}'")
+        
+ListBucket()
