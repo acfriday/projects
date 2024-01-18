60f3f7f05a0238a18fa43ba9e85c223e074a0d7a
ACF <82184168+acfriday@users.noreply.github.com>
2024-01-18 09:48:03 -0500

    updated syntax

--- aws/s3/s3-list-buckets.py
+++ aws/s3/s3-list-buckets.py
@@ -1,11 +1,14 @@
-import os
 import boto3
 from botocore.exceptions import ClientError
-os.environ["AWS_Profile"] = "projects"
 
+service = "s3"
+region = "us-east-1"
+profile = "projects"
+
+boto3 = boto3.session.Session(profile_name=profile)
 def ListBucket():
     try:
-        s3 = boto3.client('s3')
+        s3 = boto3.client(service_name=service,region_name=region)
         response = s3.list_buckets()
         if not response:
              print("No buckets available to this user")
